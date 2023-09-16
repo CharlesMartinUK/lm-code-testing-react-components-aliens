4346896b1e28eba2a606483bfaac14248594a79e
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-16 20:32:07 +0100

    Added reason tests

---
+++ src/components/reason.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import Reason from './reason';
+import user from '@testing-library/user-event';
+
+test('renders reason element', () => {
+	
+	const props ={
+		"reason":"cats rules",
+		onChangeReason:jest.fn(),
+	};
+	
+	render(<Reason {...props} />);
+
+	const sn = screen.getByLabelText("Reason for sparing")
+	expect(sn).toBeInTheDocument()
+	
+});
+
+
+test('default reason set correctly', () => {
+	
+	const props ={
+		"reason":"cats rules",
+		onChangeReason:jest.fn(),
+	};
+	
+	render(<Reason {...props} />);
+
+	const st = screen.getByLabelText("Reason for sparing")
+	expect(st).toHaveValue("cats rules")
+	
+});
+
+
+test('reason change handler called', async () =>  {
+	
+	const mockChange = jest.fn()
+	
+	const props ={
+		"reason":"cats rules",
+		onChangeReason:mockChange,
+	};
+	
+	render(<Reason {...props} />);
+	
+	await user.type(screen.getByLabelText("Reason for sparing"), ' and catnip');
+
+	const st = screen.getByLabelText("Reason for sparing");
+	expect(mockChange).toHaveBeenCalledTimes(11)
+	
+});
