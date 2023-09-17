b49e7d385684443c3ef2b1ee0c86e11b64f504da
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-17 10:47:47 +0100

    Minor improvements to question validation

--- src/validate/validate_question.ts
+++ src/validate/validate_question.ts
@@ -1,7 +1,7 @@
 
 
 // validate_species_name.ts
-const validateQuestion : (question : string) => string[] = (question) => {
+const validateQuestion : (answer : string) => string[] = (answer) => {
 	// validation code here
 	// needs to return an array of error messages
 	// or an empty array if there aren't any
@@ -9,8 +9,8 @@ const validateQuestion : (question : string) => string[] = (question) => {
 	const errors:string[] = []
 	
 		
-	if(question != "4")
-		errors.push("invalid answer")
+	if(answer != "4")
+		errors.push("Invalid answer")
 	
 	
 	//no errors
