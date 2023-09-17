fcb100f41b176386bbcc7dccb8378eaec2fcb8c9
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-17 14:17:06 +0100

    Added tests for validation called by planet name

--- src/components/planet_name.test.tsx
+++ src/components/planet_name.test.tsx
@@ -55,3 +55,53 @@ test('PlanetName change handler called', async () =>  {
 	expect(mockChange).toHaveBeenCalledTimes(12)
 	
 });
+
+
+test('PlanetName too short validation correctly', () => {
+	
+	const mockValidate = jest.fn()
+	
+	const props ={
+		"planetName":"Donutopia",
+		onChangePlanetName:jest.fn(),
+		validate:mockValidate,
+		value:"Donutopia"
+	};
+	
+	mockValidate.mockReturnValue(["Planet name too short"])
+	
+	render(<PlanetName {...props} />);
+	
+	const t = screen.getByText('Planet name too short') 
+	
+	//console.log(t)  
+	expect(t).toBeInTheDocument()
+	
+})
+
+
+test('PlanetName no problem validation correctly', () => {
+	
+	const mockValidate = jest.fn()
+	
+	const props ={
+		"planetName":"Donutopia",
+		onChangePlanetName:jest.fn(),
+		validate:mockValidate,
+		value:"Donutopia"
+	};
+	
+	mockValidate.mockReturnValue([])
+	
+	const {container} = render(<PlanetName {...props} />);
+	
+	const t = container.getElementsByClassName('error') 
+	
+	//console.log(t)  
+	expect(t).toHaveLength(0);
+	
+})
+
+
+
+
